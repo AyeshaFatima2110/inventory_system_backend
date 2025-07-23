ed068690773cf3c883ff7e05f6ef26d4a376cedc
ayesha fatima <ayeshahaneef21@gmail.com>
2025-07-23 19:02:51 +0500

    Set up global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nest-knexjs';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+      ConfigModule.forRoot({ 
+      cache: true,
+      isGlobal: true }),
        KnexModule.forRootAsync({
       useFactory: () => ({
         config: {
