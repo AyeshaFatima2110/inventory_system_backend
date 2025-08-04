bac66dd6a2c4b10b610f6f56afdf147c40594983
ayesha fatima <ayeshahaneef21@gmail.com>
2025-08-04 18:02:44 +0500

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,18 @@
+import { BaseModel } from "./base.model";
+
+export class User extends BaseModel{
+  static tableName: string = 'users';
+  static idColumn: string = 'user_id';
+
+  uuid!: string;
+  userId: number;
+  firstName : string;
+  lastName : string;
+  username : string;
+  password : string;
+  email : string; 
+  roleId : string;
+  deletedAt : null | string;
+
+
+}
