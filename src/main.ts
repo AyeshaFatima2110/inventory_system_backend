3ae66acb8e7caa3081b43c478b76fd6dbe719f3d
ayesha fatima <ayeshahaneef21@gmail.com>
2025-08-08 17:44:32 +0500

    integrate swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule , DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
     credentials: true, // Allow cookies to be sent
     allowedHeaders: 'Content-Type, Authorization', // Permitted request headers
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management System Example')
+    .setDescription('The inventory management system API')
+    .setVersion('1.0')
+    .addTag('Inventory management system')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app , config);
+  SwaggerModule.setup('api', app , documentFactory);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
